41fac9ff618d5633a15a1830ba280ed817812377
Bode Falade <bode.falade@outlook.com>
2019-02-06 14:24:15 -0600

    hashing and salt added to secure passwords.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     console.log('req.user:', req.user);
     
     pool.query(`SELECT * FROM "secret" 
-                WHERE "secret"."secrecy_level" <= req.use.clearance_level;`)
+                WHERE "secret"."secrecy_level" <= ${req.user.clearance_level};`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
